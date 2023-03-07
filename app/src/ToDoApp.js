a6652133d158c721c439eeed08c38ce50c862baf
keenset1 <laurent14511@gmail.com>
2023-03-06 20:12:39 -0500

    lolo

--- app/src/ToDoApp.js
+++ app/src/ToDoApp.js
@@ -1,49 +1,90 @@
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-const ToDoApp = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+class ToDoApp extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleSubmit = (event) => {
+    // Define initial state with an empty array of items and an empty string for the new item text
+    this.state = {
+      items: [],
+      newItemText: ''
+    };
+
+    // Bind event handlers to this instance of the component
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  // Event handler for input change updates the state with the new text input value
+  handleInputChange(event) {
+    this.setState({ newItemText: event.target.value });
+  }
+
+  // Event handler for form submission creates a new item object with a unique ID and the new text input value
+  // The new item is added to the items array in state, and the new item text is reset to an empty string
+  handleSubmit(event) {
     event.preventDefault();
-    setTodos([...todos, newTodo]);
-    setNewTodo('');
-  };
 
-  const handleChange = (event) => {
-    setNewTodo(event.target.value);
-  };
+    if (this.state.newItemText.trim() === '') {
+      // If the new item text is empty or only whitespace, do nothing
+      return;
+    }
 
-  const handleRemove = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
+    const newItem = {
+      id: Date.now(),
+      text: this.state.newItemText
+    };
 
-  return (
-    <div className="todo-app">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newTodo}
-          onChange={handleChange}
-          placeholder="Add "
-        />
-        <button className='button' type="submit">Add</button>
-      </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <button onClick={() => handleRemove(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem],
+      newItemText: ''
+    }));
+  }
+
+  // Event handler for item deletion filters out the item with the specified ID from the items array in state
+  handleDelete(itemId) {
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => item.id !== itemId)
+    }));
+  }
+
+  render() {
+    // Destructure the items and newItemText values from state
+    const { items, newItemText } = this.state;
+
+    return (
+      <div className="todo">
+        <h1>To-Do List</h1>
+        <form onSubmit={this.handleSubmit}>
+          {/* Input field for adding a new item */}
+          <input
+            type="text"
+            placeholder="Add an item"
+            value={newItemText}
+            onChange={this.handleInputChange}
+          />
+          {/* Submit button for adding a new item */}
+          <button type="submit">Add</button>
+        </form>
+        {/* List of items */}
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {/* Text of the item */}
+              {item.text}
+              {/* Button to delete the item */}
+              <button onClick={() => this.handleDelete(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ToDoApp;
+
 
